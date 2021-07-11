d5ef39cef6872eaac72d7ec230e3b1f5c950a12a
Lukas Le <longlhh90@google.com>
2021-07-10 22:39:15 -0600

    [app][index] cleanup index

--- src/index.js
+++ src/index.js
@@ -5,12 +5,19 @@ const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
+const urls = require('./urls');
+const { exception_handler, not_found_exception } = require('./middleware/error_handlers.middleware')
 
 // defining database 
 const { startDatabase } = require('./database/db_mongo');
 
 // defining the Express app
 const app = express();
+
+// get config vars
+dotenv.config();
+// access config var
+const APP_PORT = process.env.APP_PORT;
 
 
 // start the in-memory MongoDB instance
@@ -20,44 +27,19 @@ startDatabase().then(async () => {
 
 // adding Helmet to enhance your API's security
 app.use(helmet());
-
-
 // using bodyParser to parse JSON bodies into JS objects
 app.use(bodyParser.json());
-
 // enabling CORS for all requests
 app.use(cors());
-
 // adding morgan to log HTTP requests
 app.use(morgan('combined'));
 
-// get config vars
-dotenv.config();
+// adding endpoint urls
+app.use('/', urls);
 
-// access config var
-const APP_PORT = process.env.APP_PORT;
-
-// TODO: move to routes/urls
-// defining endpoints
-var usersRouter = require('./routes/users.routes');
-
-app.use('/users', usersRouter);
-
-
-// TODO: move them to middleware
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    res.status(err.status || 404).json({
-        message: "No such route exists"
-    })
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-    res.status(err.status || 500).json({
-        message: "Error Message"
-    })
-});
+// errors handling
+app.use(exception_handler);
+app.use(not_found_exception);
 
 
 // starting the server
